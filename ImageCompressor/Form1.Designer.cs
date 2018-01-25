a103add88f3715fb1b19d0ff2247c513619132ad
Pedram <Pedram@DESKTOP-GPIB36C>
2018-01-25 04:25:29 +0330

    Add project files.

---
+++ ImageCompressor/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace ImageCompressor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.ofd_source = new System.Windows.Forms.OpenFileDialog();
+            this.btnSource = new System.Windows.Forms.Button();
+            this.txtSources = new System.Windows.Forms.TextBox();
+            this.btnDest = new System.Windows.Forms.Button();
+            this.txtDest = new System.Windows.Forms.TextBox();
+            this.fbDest = new System.Windows.Forms.FolderBrowserDialog();
+            this.btnSetWebPDir = new System.Windows.Forms.Button();
+            this.fbWebPDir = new System.Windows.Forms.FolderBrowserDialog();
+            this.btnProgress = new System.Windows.Forms.Button();
+            this.rdJPEG = new System.Windows.Forms.RadioButton();
+            this.rdWebp = new System.Windows.Forms.RadioButton();
+            this.btnSetJPEGTran = new System.Windows.Forms.Button();
+            this.fbJPEGTranDir = new System.Windows.Forms.FolderBrowserDialog();
+            this.SuspendLayout();
+            // 
+            // ofd_source
+            // 
+            this.ofd_source.Multiselect = true;
+            this.ofd_source.RestoreDirectory = true;
+            this.ofd_source.Title = "Select Images";
+            // 
+            // btnSource
+            // 
+            this.btnSource.Location = new System.Drawing.Point(385, 12);
+            this.btnSource.Name = "btnSource";
+            this.btnSource.Size = new System.Drawing.Size(75, 23);
+            this.btnSource.TabIndex = 0;
+            this.btnSource.Text = "Source";
+            this.btnSource.UseVisualStyleBackColor = true;
+            this.btnSource.Click += new System.EventHandler(this.btnSource_Click);
+            // 
+            // txtSources
+            // 
+            this.txtSources.Location = new System.Drawing.Point(13, 14);
+            this.txtSources.Multiline = true;
+            this.txtSources.Name = "txtSources";
+            this.txtSources.ReadOnly = true;
+            this.txtSources.Size = new System.Drawing.Size(162, 252);
+            this.txtSources.TabIndex = 1;
+            // 
+            // btnDest
+            // 
+            this.btnDest.Location = new System.Drawing.Point(385, 41);
+            this.btnDest.Name = "btnDest";
+            this.btnDest.Size = new System.Drawing.Size(75, 23);
+            this.btnDest.TabIndex = 0;
+            this.btnDest.Text = "Dest";
+            this.btnDest.UseVisualStyleBackColor = true;
+            this.btnDest.Click += new System.EventHandler(this.btnDest_Click);
+            // 
+            // txtDest
+            // 
+            this.txtDest.Location = new System.Drawing.Point(181, 70);
+            this.txtDest.Multiline = true;
+            this.txtDest.Name = "txtDest";
+            this.txtDest.ReadOnly = true;
+            this.txtDest.Size = new System.Drawing.Size(279, 38);
+            this.txtDest.TabIndex = 2;
+            // 
+            // btnSetWebPDir
+            // 
+            this.btnSetWebPDir.Location = new System.Drawing.Point(369, 243);
+            this.btnSetWebPDir.Name = "btnSetWebPDir";
+            this.btnSetWebPDir.Size = new System.Drawing.Size(91, 23);
+            this.btnSetWebPDir.TabIndex = 3;
+            this.btnSetWebPDir.Text = "Set WebP Dir";
+            this.btnSetWebPDir.UseVisualStyleBackColor = true;
+            this.btnSetWebPDir.Click += new System.EventHandler(this.btnSetWebPDir_Click);
+            // 
+            // btnProgress
+            // 
+            this.btnProgress.Location = new System.Drawing.Point(181, 114);
+            this.btnProgress.Name = "btnProgress";
+            this.btnProgress.Size = new System.Drawing.Size(75, 23);
+            this.btnProgress.TabIndex = 4;
+            this.btnProgress.Text = "Progress";
+            this.btnProgress.UseVisualStyleBackColor = true;
+            this.btnProgress.Click += new System.EventHandler(this.btnProgress_Click);
+            // 
+            // rdJPEG
+            // 
+            this.rdJPEG.AutoSize = true;
+            this.rdJPEG.Checked = true;
+            this.rdJPEG.Location = new System.Drawing.Point(181, 15);
+            this.rdJPEG.Name = "rdJPEG";
+            this.rdJPEG.Size = new System.Drawing.Size(52, 17);
+            this.rdJPEG.TabIndex = 5;
+            this.rdJPEG.TabStop = true;
+            this.rdJPEG.Text = "JPEG";
+            this.rdJPEG.UseVisualStyleBackColor = true;
+            // 
+            // rdWebp
+            // 
+            this.rdWebp.AutoSize = true;
+            this.rdWebp.Location = new System.Drawing.Point(182, 39);
+            this.rdWebp.Name = "rdWebp";
+            this.rdWebp.Size = new System.Drawing.Size(54, 17);
+            this.rdWebp.TabIndex = 6;
+            this.rdWebp.Text = "Webp";
+            this.rdWebp.UseVisualStyleBackColor = true;
+            // 
+            // btnSetJPEGTran
+            // 
+            this.btnSetJPEGTran.Location = new System.Drawing.Point(256, 243);
+            this.btnSetJPEGTran.Name = "btnSetJPEGTran";
+            this.btnSetJPEGTran.Size = new System.Drawing.Size(107, 23);
+            this.btnSetJPEGTran.TabIndex = 3;
+            this.btnSetJPEGTran.Text = "Set JPEGTran Dir";
+            this.btnSetJPEGTran.UseVisualStyleBackColor = true;
+            this.btnSetJPEGTran.Click += new System.EventHandler(this.btnSetJPEGTranDir_Click);
+            // 
+            // fbJPEGTranDir
+            // 
+            this.fbJPEGTranDir.HelpRequest += new System.EventHandler(this.fbJPEGTranDir_HelpRequest);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(472, 278);
+            this.Controls.Add(this.rdWebp);
+            this.Controls.Add(this.rdJPEG);
+            this.Controls.Add(this.btnProgress);
+            this.Controls.Add(this.btnSetJPEGTran);
+            this.Controls.Add(this.btnSetWebPDir);
+            this.Controls.Add(this.txtDest);
+            this.Controls.Add(this.txtSources);
+            this.Controls.Add(this.btnDest);
+            this.Controls.Add(this.btnSource);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Web Compression";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog ofd_source;
+        private System.Windows.Forms.Button btnSource;
+        private System.Windows.Forms.TextBox txtSources;
+        private System.Windows.Forms.Button btnDest;
+        private System.Windows.Forms.TextBox txtDest;
+        private System.Windows.Forms.FolderBrowserDialog fbDest;
+        private System.Windows.Forms.Button btnSetWebPDir;
+        private System.Windows.Forms.FolderBrowserDialog fbWebPDir;
+        private System.Windows.Forms.Button btnProgress;
+        private System.Windows.Forms.RadioButton rdJPEG;
+        private System.Windows.Forms.RadioButton rdWebp;
+        private System.Windows.Forms.Button btnSetJPEGTran;
+        private System.Windows.Forms.FolderBrowserDialog fbJPEGTranDir;
+    }
+}
+
---
+++ ImageCompressor/Form1.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace ImageCompressor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.AllowDrop = true;
+            this.DragEnter += new DragEventHandler(Form1_DragEnter);
+            this.DragDrop += new DragEventHandler(Form1_DragDrop);
+            ImageList = new List<string>();
+        }
+
+        void Form1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop,false))
+            {
+                e.Effect = DragDropEffects.All;
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None;
+            }
+        }
+
+        void Form1_DragDrop(object sender, DragEventArgs e)
+        {
+           string[] WhiteExtension = { ".png",".jpg",".jpeg" };
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop, false);
+            foreach (string file in files)
+            {
+                string ext = Path.GetExtension(file);
+                //if its image
+                if (WhiteExtension.Where(y=>y==ext.ToLower()).Any())
+                {
+                    ImageList.Add(file);
+                }
+            }
+            SourceChanged();
+        }
+        public List<string> ImageList { get; set; }
+        private void btnSource_Click(object sender, EventArgs e)
+        {
+            
+            if (ofd_source.ShowDialog() == DialogResult.OK)
+            {
+                if (ofd_source.FileNames!=null)
+                {
+                    ImageList.AddRange(ofd_source.FileNames);
+                }
+                SourceChanged();
+            }
+        }
+
+        private void SourceChanged()
+        {
+            txtSources.Text = "";
+            foreach (var item in ImageList)
+            {
+                txtSources.Text += item + Environment.NewLine;
+            }
+        }
+
+        private void btnSetWebPDir_Click(object sender, EventArgs e)
+        {
+            if (fbWebPDir.ShowDialog()== DialogResult.OK)
+            {
+                SetValueWebp(fbWebPDir.SelectedPath);
+                MessageBox.Show("Path Saved");
+            }
+        }
+        private void btnSetJPEGTranDir_Click(object sender, EventArgs e)
+        {
+            if (fbJPEGTranDir.ShowDialog() == DialogResult.OK)
+            {
+                SetValueJPEGTran(fbJPEGTranDir.SelectedPath);
+                MessageBox.Show("Path Saved");
+            }
+        }
+        private void SetValueWebp(string val)
+        {
+            Environment.SetEnvironmentVariable("ImgWebpCompressor", val, EnvironmentVariableTarget.User);
+        }
+        private string GetValueWebp()
+        {
+            return Environment.GetEnvironmentVariable("ImgWebpCompressor", EnvironmentVariableTarget.User);
+        }
+        private void SetValueJPEGTran(string val)
+        {
+            Environment.SetEnvironmentVariable("ImgJPEGTranCompressor", val, EnvironmentVariableTarget.User);
+        }
+        private string GetValueJPEGTran()
+        {
+            return Environment.GetEnvironmentVariable("ImgJPEGTranCompressor", EnvironmentVariableTarget.User);
+        }
+        private void btnDest_Click(object sender, EventArgs e)
+        {
+            if (fbDest.ShowDialog() == DialogResult.OK)
+            {
+                txtDest.Text = fbDest.SelectedPath;
+            }
+        }
+
+        private void btnProgress_Click(object sender, EventArgs e)
+        {
+            if (rdWebp.Checked)
+            {
+                foreach (var item in ImageList)
+                {
+                    compressWebp(item);
+                }
+            }
+            else
+            {
+                foreach (var item in ImageList)
+                {
+                    compressJPEG(item);
+                }
+            }
+            openExplore(txtDest.Text);
+            ImageList = new List<string>();
+            SourceChanged();
+        }
+        private void openExplore(string dest)
+        {
+            var process = new ProcessStartInfo("cmd.exe");
+            string command = String.Format(@"/c explorer {0}", dest);
+            process.Arguments = command;
+            process.ErrorDialog = true;
+            process.UseShellExecute = false;
+            process.RedirectStandardOutput = true;
+            process.RedirectStandardError = true;
+            process.CreateNoWindow = true;
+            Process.Start(process);
+        }
+        private void compressJPEG(string source)
+        {
+            if (string.IsNullOrEmpty(GetValueJPEGTran()))
+            {
+                MessageBox.Show("Set JPEGTran Directory");
+                return;
+            }
+            else
+            {
+                string tempFileName = "jpegtran.exe";
+                FileInfo fi = new FileInfo(source);
+                var dir = fi.Directory.FullName;
+                var name = Path.GetFileNameWithoutExtension(fi.Name);
+                string libpath = GetValueJPEGTran();
+                var process = new ProcessStartInfo("cmd.exe");
+                string command = String.Format(@"/c {0}\{1} -optimize -progressive {2} {3}\{4}.jpg", libpath, tempFileName, source, txtDest.Text, name);
+                process.Arguments = command;
+                process.ErrorDialog = true;
+                process.UseShellExecute = false;
+                process.RedirectStandardOutput = true;
+                process.RedirectStandardError = true;
+                process.CreateNoWindow = true;
+                Process.Start(process);
+            }
+        }
+        private void compressWebp(string source)
+        {
+            if (string.IsNullOrEmpty(GetValueWebp()))
+            {
+                MessageBox.Show("Set WebP Directory");
+                return;
+            }
+            else
+            {
+                string tempFileName = "cwebp.exe";
+                FileInfo fi = new FileInfo(source);
+                var dir = fi.Directory.FullName;
+                var name = Path.GetFileNameWithoutExtension(fi.Name);
+                string libpath = GetValueWebp();
+                var process = new ProcessStartInfo("cmd.exe");
+                string command = String.Format(@"/c {0}\{1} {2} -q 80 -o {3}\{4}.webp", libpath,tempFileName,source, txtDest.Text,name);
+                process.Arguments = command;
+                process.ErrorDialog = true;
+                process.UseShellExecute = false;
+                process.RedirectStandardOutput = true;
+                process.RedirectStandardError = true;
+                process.CreateNoWindow = true;
+                Process.Start(process);
+            }
+        }
+
+        private void fbJPEGTranDir_HelpRequest(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
